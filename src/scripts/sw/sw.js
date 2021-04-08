437e09c89c2394061205d67fabcb70db41805bab
Heydar Shams <shamsheidar.79@gmail.com>
2021-04-09 01:22:15 +0430

    add "install" event listener

--- src/scripts/sw/sw.js
+++ src/scripts/sw/sw.js
@@ -1,8 +1,10 @@
 const ASSETS_LIST = self.__WB_MANIFEST // eslint-disable-line
 const CACHE_NAME = 'breathe-app-main-cache' // eslint-disable-line
+const ENV = __ENV__
 
 self.addEventListener('install', event => {
   console.log('SW: install')
+  event.waitUntil(precache(ASSETS_LIST, CACHE_NAME))
 })
 
 self.addEventListener('activate', event => {
@@ -13,3 +15,18 @@ self.addEventListener('fetch', event => {
   console.log('SW: fetch')
   event.respondWith(fetch(event.request))
 })
+
+function precache (assetsList, cacheName) {
+  return caches.open(cacheName).then(cache => {
+    const urlsList = assetsList.map(asset => {
+      return asset.url
+    })
+
+    return cache.addAll(urlsList)
+      .then(value => {
+        console.log(`SW: precache ${urlsList.length} entries in "${cacheName}"`)
+      })
+      .catch(error => console.log(error))
+  })
+}
+
