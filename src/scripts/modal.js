3bd23beb1e3e3393c5626dcae250a80f96f72935
--global <shamsheidar.79@gmail.com>
2021-10-20 14:19:31 +0330

    add logic for modal scroll shadows

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,8 +2,11 @@ const overlay = document.querySelector('.overlay')
 
 export function openModal (modal) {
   const btn = modal.querySelector('.modal__button')
+  const modalOverlay = modal.querySelector('.modal__overlay')
   const body = modal.querySelector('.modal__body')
   btn.addEventListener('click', btnClickHandler)
+  body.addEventListener('scroll', scrollHandler)
+  setModalShadow(modalOverlay, body)
   modal.classList.add('modal--open')
   overlay.classList.add('overlay--open')
 }
@@ -12,6 +15,7 @@ function closeModal (modal) {
   const btn = modal.querySelector('.modal__button')
   const body = modal.querySelector('.modal__body')
   btn.removeEventListener('click', btnClickHandler)
+  body.removeEventListener('scroll', scrollHandler)
   modal.classList.remove('modal--open')
   overlay.classList.remove('overlay--open')
 }
@@ -21,3 +25,34 @@ function btnClickHandler (event) {
   closeModal(modal)
 }
 
+function scrollHandler (event) {
+  const body = event.target
+  const overlay = body.parentElement
+  setModalShadow(overlay, body)
+}
+
+function setModalShadow (overlay, body) {
+  const offset = 50
+  const scrollMax = body.scrollHeight - body.clientHeight
+  const scroll = body.scrollTop
+  const style = overlay.style
+
+  if (scroll < offset) {
+    style.setProperty('--top-opacity', scroll / offset)
+    if (style.getPropertyValue('--bottom-opacity') !== '1') {
+      style.setProperty('--bottom-opacity', 1)
+    }
+  } else if (scroll > scrollMax - offset) {
+    if (style.getPropertyValue('--top-opacity') !== '1') {
+      style.setProperty('--top-opacity', 1)
+    }
+    style.setProperty('--bottom-opacity', (-scroll + scrollMax) / offset)
+  } else {
+    if (style.getPropertyValue('--bottom-opacity') !== '1') {
+      style.setProperty('--bottom-opacity', 1)
+    }
+    if (style.getPropertyValue('--top-opacity') !== '1') {
+      style.setProperty('--top-opacity', 1)
+    }
+  }
+}
